c8ea635bc4e942d0fe3aff21a4bec672ceaabd6e
Paul Nelson <pmnelso@gmail.com>
2019-05-01 14:58:49 -0400

    added 10 products to database

--- bamazon.sql
+++ bamazon.sql
@@ -13,4 +13,34 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Acoustic Guitar", "Music", 350.00, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("13in Macbook Pro", "Electronics", 1100.00, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Garmin Fenix 5 GPS Watch", "Electronics", 350.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Taylormade M5 Driver", "Golf", 525.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Sage Fly Fishing Rod", "Outdoors", 800.00, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Simms Waders", "Outdoors", 250.00, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Wilson Football", "Sporting Goods", 19.99, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("10in Cast Iron Skillet", "Kitchen Supplies", 30.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Stainless Steel Toaster", "Kitchen Supplies", 33.75, 18);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Milwaukee 18V Cordless Drill", "Tools", 41.76, 9);
+
 SELECT * FROM products;
